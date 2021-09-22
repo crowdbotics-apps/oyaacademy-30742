b937f819a8fb489efead9f74c773c9f7f448e4f9
Crowdbotics <team@crowdbotics.com>
2021-09-22 08:34:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/oyaacademy_30742/MainActivity.java
@@ -0,0 +1,15 @@
+package com.oyaacademy_30742;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "oyaacademy_30742";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "oyaacademy_30742Identifier",
+  url: "https://oyaacademy_30742Identifier.botics.co",
+  api: "https://oyaacademy_30742Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
